b1296a30a7e8423ebd61edab009d39dba474ee8a
coolbele <64392071+coolbele@users.noreply.github.com>
2020-06-02 14:49:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,5 +11,5 @@ sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_genera
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # Delete default password
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/default-settings/files/zzz-default-settings
+#sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/default-settings/files/zzz-default-settings
 
